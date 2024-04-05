818492e69b8ba8dd92555b0d4d20f8d02de6c08c
qkudev <qkudev@gmail.com>
2024-04-05 20:43:46 +0200

    jest: key bindings

--- lua/plugins/jest.lua
+++ lua/plugins/jest.lua
@@ -1,3 +1,45 @@
 return {
   "mattkubej/jest.nvim",
+  keys = {
+    {
+      "<leader>jf",
+      function()
+        require("nvim-jest").test_file()
+      end,
+      desc = "Jest File",
+    },
+    {
+      "<leader>js",
+      function()
+        require("nvim-jest").test_single()
+      end,
+      desc = "Jest Single",
+    },
+    {
+      "<leader>jp",
+      function()
+        require("nvim-jest").test_project()
+      end,
+      desc = "Jest Project",
+    },
+    {
+      "<leader>jc",
+      function()
+        require("nvim-jest").test_coverage()
+      end,
+      desc = "Jest Coverage",
+    },
+  },
+  dependencies = {
+    -- which key integration
+    {
+      "folke/which-key.nvim",
+      optional = true,
+      opts = {
+        defaults = {
+          ["<leader>j"] = { name = "+jest" },
+        },
+      },
+    },
+  },
 }
