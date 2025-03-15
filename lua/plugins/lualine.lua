45e034520e9ee2201f9ac907b34ece6cf640a7d7
qkudev <qkudev@gmail.com>
2025-03-15 11:58:39 +0100

    improve: lualine with lsp, diagnostics and datetime

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,17 @@ return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   opts = function(_, opts)
-    table.insert(opts.sections.lualine_x, "😄")
+    table.insert(opts.sections.lualine_c, "diff")
+
+    local datetime = {
+      function()
+        return os.date("%H:%M %d.%m.%Y")
+      end,
+      icon = "",
+    }
+
+    opts.sections.lualine_x = { "lsp_status" }
+    opts.sections.lualine_y = { "diagnostics" }
+    opts.sections.lualine_z = { datetime }
   end,
 }
